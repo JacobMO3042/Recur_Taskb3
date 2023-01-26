f18d2bd64d1c7097b6a7f1ed334c259e36f1a9ab
JacobMO3042 <115160743+JacobMO3042@users.noreply.github.com>
2023-01-26 15:14:39 +0000

    leaves addd

--- Recur_task/js/recursion3.js
+++ Recur_task/js/recursion3.js
@@ -1,7 +1,7 @@
 // THIS METHOD MOVES THE CANVAS TO THE BRANCHING POINT??
 let green1, green2, slider;
-function setup(){
-    createCanvas(windowWidth,windowHeight);
+function setup() {
+    createCanvas(windowWidth, windowHeight);
     angleMode(DEGREES);
     noLoop();
     strokeJoin(ROUND);
@@ -9,37 +9,42 @@ function setup(){
     green2 = color('#50B86B');
     slider = createSlider(10, 90, 45)
     slider.position(10, windowHeight - 40);
-    slider.style("width","300px");
+    slider.style("width", "300px");
     slider.input(draw);
 }
-function draw(){
+function draw() {
     background(220);
     resetMatrix();
     translate(width / 2, height);
     branch(250)
 
 }
-function branch(len){ //length of pranch to draw
+function branch(len) { //length of pranch to draw
     let maxAngle = slider.value();
     strokeWeight(map(len, 20, 100, 1, 10));
-    stroke(lerpColor(green1,green2, random(0.3,0.7)));
-    line(0, 0,  0, - len);
+    stroke(lerpColor(green1, green2, random(0.3, 0.7)));
+    line(0, 0, 0, - len);
     translate(0, -len);
-    
-    if (len > 20){
-        if(len < 50){
+
+    if (len > 20) {
+        if (len < 50) {
             //leaves 
-            let r = 40 + random(-20,20);
+            let r = 40 + random(-20, 20);
             let g = 200 + random(-20, 20);
             let b = 40 + random(-20, 20);
-            fill(r,g,b, 100);
+            fill(r, g, b, 100);
             let size = 15 + random(15);
             noStroke();
             push()
             rotate(random(-20, 90))
-            ellipse(0,-size * 1.5 ,size,size * 3)
+            // ellipse(0,-size * 1.5 ,size,size * 3)
             pop()
-            // triangle(-size/2, 0, size/2, 0, 0, -size );
+            triangle(-size / 2, 0, size / 2, 0, 0, -size);
+            rotate(-45, 45);
+            triangle(-size / 4, 0, size / 4, 0, 0, -size);
+            rotate(90, 90);
+            triangle(-size / 4, 0, size / 4, 0, 0, -size);
+
             // beginShape();
             // let radius = random(10, 30);
             // for(let i = 45; i< 135; i++){
@@ -53,23 +58,22 @@ function branch(len){ //length of pranch to draw
             //     vertex(x, y);
             // }
             // endShape(CLOSE)
-
-        }else{
-                    //branch 1
-        push();
-        rotate(random(-maxAngle, maxAngle));
-        branch(len * 0.4);
-        pop();
-        //branch 2
-        push();
-        rotate(random(-maxAngle, maxAngle));
-        branch(len * 0.9);
-        pop();
+        } else {
+            //branch 1
+            push();
+            rotate(random(-maxAngle, maxAngle));
+            branch(len * 0.4);
+            pop();
+            //branch 2
+            push();
+            rotate(random(-maxAngle, maxAngle));
+            branch(len * 0.9);
+            pop();
             //branch 3
-               push();
-               rotate(random(-maxAngle, maxAngle));
-               branch(len * 0.5);
-               pop();
+            push();
+            rotate(random(-maxAngle, maxAngle));
+            branch(len * 0.5);
+            pop();
         }
 
     }
